45dbe490a0edd6331873ce79606f3e0f0b972fc1
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-01-29 18:06:04 -0300

    req3 feito

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    Função recursiva para verificar se uma palavra é um palíndromo.
+
+    Args:
+        word: A palavra a ser verificada.
+        low_index: O índice inicial da palavra.
+        high_index: O índice final da palavra.
+
+    Returns:
+        True se a palavra for um palíndromo, False caso contrário.
+    """
+    if len(word) == 0:
+        return False
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
