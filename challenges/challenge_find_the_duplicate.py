c86d78f84a37d12d545302a2ceda24acde30a3a9
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-01-30 15:48:47 -0300

    reqs bonus feitos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Função para encontrar os números que se repetem da lista."""
+
+    unique_numbers = set()
+    for num in nums:
+        if type(num) is not int or num < 0:
+            return False
+
+        if num in unique_numbers:
+            return num
+        else:
+            unique_numbers.add(num)
+
+    return False
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Função para verificar se uma palavra é um palíndromo."""
+    if len(word) == 0:
+        return False
+
+    # solução com slicing
+    if word != word[::-1]:
+        return False
+    return True
+
+    # solução com range
+    # for i in range(len(word)):
+    #     if word[i] != word[len(word) - 1 - i]:
+    #         return False
+    # return True
